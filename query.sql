1d22b56346563bde05d248fcf809a8bc3ef000f8
WAbreu738 <wesleyabreu738@gmail.com>
2024-04-18 17:19:14 -0400

    popstgres database review

---
+++ query.sql
@@ -0,0 +1,15 @@
+
+SELECT * FROM courses;
+
+-- SELECT 
+--         students.id AS student_id,
+--         CONCAT(fire_name, '', last_name) AS full_name,
+--         course_id,
+--         name,
+--         type
+--     FROM students
+--     JOIN courses
+--     ON students.course_id = courses.id;
+    -- WHERE courses.id = 3;
+
+
